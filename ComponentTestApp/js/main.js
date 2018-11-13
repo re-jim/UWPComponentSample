3aede5297118b12c51c29e8bd5c1fde1865e59b2
Grant Curtis <grant@realeyes.com>
2018-11-12 20:03:25 -0700

    initial commit

---
+++ ComponentTestApp/js/main.js
@@ -0,0 +1,1 @@
+﻿console.log(RuntimeComponent1.Example.doSomething())
---
+++ RuntimeComponent1/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeComponent1
+{
+    public sealed class Example
+    {
+        public static string DoSomething()
+        {
+            return "hello world";
+        }
+    }
+}
